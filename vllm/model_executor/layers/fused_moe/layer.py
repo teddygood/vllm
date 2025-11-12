1fc51eeddfe58f2cf1a86347215deb22c25afe14
teddygood <ibear6954@gmail.com>
2025-11-13 02:08:58 +0900

    Add naive prepare finalize pipeline
    Signed-off-by: teddygood <ibear6954@gmail.com>
    
    # Conflicts:
    #	vllm/model_executor/layers/fused_moe/fused_moe_method_base.py
    #	vllm/model_executor/layers/fused_moe/fused_moe_modular_method.py
    #	vllm/model_executor/layers/fused_moe/layer.py
    #	vllm/model_executor/layers/fused_moe/modular_kernel.py

--- vllm/model_executor/layers/fused_moe/layer.py
+++ vllm/model_executor/layers/fused_moe/layer.py
@@ -1924,6 +1924,32 @@ class FusedMoE(CustomOp):
         do_naive_dispatch_combine: bool = self.dp_size > 1 and not isinstance(
             self.quant_method, FusedMoEModularMethod
         )
+        # If there are shared experts but we are not using a modular kernel, the
+        # shared experts must be called here
+        if has_separate_shared_experts:
+            assert self.shared_experts is not None
+
+            if self.shared_experts_stream is not None:
+                # Clone BEFORE switching streams to avoid race condition
+                # where routed_expert kernel may mutate hidden_states.
+                hidden_states_clone = hidden_states.clone()
+                self.shared_experts_stream.wait_stream(current_stream())
+
+                # Run shared experts in parallel on a separate stream
+                with torch.cuda.stream(self.shared_experts_stream):
+                    shared_output = self.shared_experts(hidden_states_clone)
+
+                # Record that the clone will be used by shared_experts_stream
+                # to avoid gc issue from deallocation of hidden_states_clone
+                # For more details: https://docs.pytorch.org/docs/stable/generated/torch.Tensor.record_stream.html # noqa: E501
+                # NOTE: we dont need shared_output.record_stream(current_stream())
+                # because we synch the streams before using shared_output.
+                hidden_states_clone.record_stream(self.shared_experts_stream)
+
+            else:
+                shared_output = self.shared_experts(hidden_states)
+        else:
+            shared_output = None
 
         ctx = get_forward_context()
         sp_ctx = (
@@ -1933,22 +1959,18 @@ class FusedMoE(CustomOp):
         )
 
         with sp_ctx:
+            hidden_states_dispatch = hidden_states
             if do_naive_dispatch_combine:
-                hidden_states_combined, router_logits = get_ep_group().dispatch(
+                hidden_states_dispatch, router_logits = get_ep_group().dispatch(
                     hidden_states, router_logits, self.is_sequence_parallel
                 )
-            # Run shared experts before matrix multiply.
-            # because matrix multiply maybe modify the hidden_states.
-            if has_separate_shared_experts and not use_shared_experts_stream:
-                assert self.shared_experts is not None
-                shared_output = self.shared_experts(hidden_states)
 
             # NOTE: Similar with DP, PCP also needs dispatch and combine. For
             # simplicity, AgRsAll2All was added separately for PCP here. Maybe
             # we should modify All2AllManager abstract to better support PCP.
             if self.pcp_size > 1:
-                hidden_states = get_pcp_group().all_gather(
-                    hidden_states,
+                hidden_states_dispatch = get_pcp_group().all_gather(
+                    hidden_states_dispatch,
                     dim=0,
                 )
                 router_logits = get_pcp_group().all_gather(
@@ -1956,12 +1978,16 @@ class FusedMoE(CustomOp):
                     dim=0,
                 )
 
+            # Run shared experts before matrix multiply.
+            # because matrix multiply maybe modify the hidden_states.
+            if has_separate_shared_experts and not use_shared_experts_stream:
+                assert self.shared_experts is not None
+                shared_output = self.shared_experts(hidden_states)
+
             # Matrix multiply.
             final_hidden_states = self.quant_method.apply(
                 layer=self,
-                x=hidden_states_combined
-                if do_naive_dispatch_combine
-                else hidden_states,
+                x=hidden_states_dispatch,
                 router_logits=router_logits,
             )
 
@@ -1988,9 +2014,11 @@ class FusedMoE(CustomOp):
                 assert isinstance(final_hidden_states, tuple)
                 final_hidden_states, zero_expert_result = final_hidden_states
 
-            def combine_output(states: torch.Tensor) -> torch.Tensor:
+            def reduce_output(states: torch.Tensor) -> torch.Tensor:
                 if do_naive_dispatch_combine:
-                    states = get_ep_group().combine(states, self.is_sequence_parallel)
+                    states = get_ep_group().combine(
+                        states, self.is_sequence_parallel
+                    )
 
                 if self.pcp_size > 1:
                     states = get_pcp_group().reduce_scatter(
@@ -1998,18 +2026,25 @@ class FusedMoE(CustomOp):
                         dim=0,
                     )
 
+                if (
+                    not self.is_sequence_parallel
+                    and self.reduce_results
+                    and (self.tp_size > 1 or self.ep_size > 1)
+                ):
+                    states = self.maybe_all_reduce_tensor_model_parallel(states)
+
                 return states
 
             if self.shared_experts is not None:
                 return (
-                    final_hidden_states[0],
-                    combine_output(final_hidden_states[1]),
+                    reduce_output(final_hidden_states[0]),
+                    reduce_output(final_hidden_states[1]),
                 )
             elif self.zero_expert_num is not None and self.zero_expert_num > 0:
                 assert isinstance(final_hidden_states, torch.Tensor)
-                return (combine_output(final_hidden_states), zero_expert_result)
+                return (reduce_output(final_hidden_states), zero_expert_result)
             else:
-                return combine_output(final_hidden_states)
+                return reduce_output(final_hidden_states)
 
     @classmethod
     def make_expert_params_mapping(
